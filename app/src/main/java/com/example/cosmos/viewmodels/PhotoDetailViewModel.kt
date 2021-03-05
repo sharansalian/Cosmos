3f01bc166326aa4d0d0460e2232400977b9f87d5
sharansalian <sharansalian.dev@gmail.com>
2021-03-05 20:22:08 +0530

    Add photo detail ViewModel that facilitates photo by id

---
+++ app/src/main/java/com/example/cosmos/viewmodels/PhotoDetailViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.cosmos.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.example.cosmos.data.PhotoRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+class PhotoDetailViewModel @AssistedInject constructor(
+    photoRepository: PhotoRepository,
+    @Assisted private val id: String
+) : ViewModel() {
+
+    val photo = photoRepository.getPhoto(id.toInt()).asLiveData()
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: PhotoDetailViewModelFactory,
+            id: String
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(id) as T
+            }
+        }
+    }
+}
+
+@AssistedFactory
+interface PhotoDetailViewModelFactory {
+    fun create(id: String): PhotoDetailViewModel
+}
