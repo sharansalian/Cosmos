4d72f2d6ea9da45c8424bc157c940f241a909828
sharansalian <sharansalian.dev@gmail.com>
2021-03-05 20:23:19 +0530

    Add photo detail fragment that binds a photo to a view and handles the scroll transition

---
+++ app/src/main/java/com/example/cosmos/PhotoDetailFragment.kt
@@ -0,0 +1,79 @@
+package com.example.cosmos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.cosmos.databinding.FragmentPhotoDetailBinding
+import com.example.cosmos.viewmodels.PhotoDetailViewModel
+import com.example.cosmos.viewmodels.PhotoDetailViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class PhotoDetailFragment : Fragment() {
+
+    private val args: PhotoDetailFragmentArgs by navArgs()
+
+    @Inject
+    lateinit var photoDetailViewModelFactory: PhotoDetailViewModelFactory
+
+    private val photoDetailViewModel: PhotoDetailViewModel by viewModels {
+        PhotoDetailViewModel.provideFactory(photoDetailViewModelFactory, args.id.toString())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = DataBindingUtil.inflate<FragmentPhotoDetailBinding>(
+            inflater,
+            R.layout.fragment_photo_detail,
+            container,
+            false
+        ).apply {
+            viewModel = photoDetailViewModel
+            lifecycleOwner = viewLifecycleOwner
+
+            var isToolbarShown = false
+
+            // scroll change listener begins at Y = 0 when image is fully collapsed
+            photoDetailScrollview.setOnScrollChangeListener(
+                NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+
+                    // User scrolled past image to height of toolbar and the title text is
+                    // underneath the toolbar, so the toolbar should be shown.
+                    val shouldShowToolbar = scrollY > toolbar.height
+
+                    // The new state of the toolbar differs from the previous state; update
+                    // appbar and toolbar attributes.
+                    if (isToolbarShown != shouldShowToolbar) {
+                        isToolbarShown = shouldShowToolbar
+
+                        // Use shadow animator to add elevation if toolbar is shown
+                        appbar.isActivated = shouldShowToolbar
+
+                        // Show the photo title if toolbar is shown
+                        toolbarLayout.isTitleEnabled = shouldShowToolbar
+                    }
+                }
+            )
+
+            toolbar.setNavigationOnClickListener { view ->
+                view.findNavController().navigateUp()
+            }
+
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+}
