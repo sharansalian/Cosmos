65fb4d20f1d2ee4aefcb1bfc34084e9f3dc1c299
sharansalian <sharansalian.dev@gmail.com>
2021-03-05 20:04:59 +0530

    Add styles

--- app/src/main/java/com/example/cosmos/data/PhotoDao.kt
+++ app/src/main/java/com/example/cosmos/data/PhotoDao.kt
@@ -1,6 +1,5 @@
 package com.example.cosmos.data
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -15,4 +14,7 @@ interface PhotoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(photos: List<Photo>)
+
+    @Query("SELECT * FROM photos WHERE id = :id")
+    fun getPhoto(id: Int): Flow<Photo>
 }
