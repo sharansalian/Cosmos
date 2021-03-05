fb99f7f6719c7d2119b0d82b5345c592468085eb
sharansalian <sharansalian.dev@gmail.com>
2021-03-05 20:19:15 +0530

    Add Binding Adapter to bindI image from URL

---
+++ app/src/main/java/com/example/cosmos/adapters/BindingAdapters.kt
@@ -0,0 +1,20 @@
+package com.example.cosmos.adapters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.cosmos.R
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .placeholder(R.drawable.ic_placholder)
+            .diskCacheStrategy( DiskCacheStrategy.ALL )
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
+}
